be5546886d651951c0fa90e144ed8d07e4a17f6d
Zamith <zamith.28@gmail.com>
2013-12-03 16:00:17 +0000

    Adds git completion to zsh

--- git-completion.bash
+++ git-completion.bash
@@ -2616,6 +2616,57 @@ __git_main ()
 	fi
 }
 
+__g_main ()
+{
+	local i c=1 command __git_dir
+
+	while [ $c -lt $cword ]; do
+		i="${words[c]}"
+		case "$i" in
+		--git-dir=*) __git_dir="${i#--git-dir=}" ;;
+		--bare)      __git_dir="." ;;
+		--help) command="help"; break ;;
+		-c) c=$((++c)) ;;
+		-*) ;;
+		*) command="$i"; break ;;
+		esac
+		((c++))
+	done
+
+	if [ -z "$command" ]; then
+		case "$cur" in
+		--*)   __gitcomp "
+			--paginate
+			--no-pager
+			--git-dir=
+			--bare
+			--version
+			--exec-path
+			--exec-path=
+			--html-path
+			--info-path
+			--work-tree=
+			--namespace=
+			--no-replace-objects
+			--help
+			"
+			;;
+		*)     __git_compute_porcelain_commands
+		       __gitcomp "$__git_porcelain_commands $(__git_aliases)" ;;
+		esac
+		return
+	fi
+
+	local completion_func="_git_${command//-/_}"
+	declare -f $completion_func >/dev/null && $completion_func && return
+
+	local expansion=$(__git_aliased_command "$command")
+	if [ -n "$expansion" ]; then
+		completion_func="_git_${expansion//-/_}"
+		declare -f $completion_func >/dev/null && $completion_func
+	fi
+}
+
 __gitk_main ()
 {
 	__git_has_doubledash && return
@@ -2738,6 +2789,11 @@ __git_complete ()
 
 # wrapper for backwards compatibility
 _git ()
+{
+	__git_wrap__git_main
+}
+
+_g ()
 {
 	__git_wrap__git_main
 }
