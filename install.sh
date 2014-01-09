c604d9f622ae428343ae53bee7233c8dcbf7b697
Zamith <zamith.28@gmail.com>
2014-01-09 12:18:20 +0000

    Using Vundle instead of pathogen

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
+# Must be ran from dotfiles root
+
 # Add aliases and functions
-ln -s "$(pwd)/aliases" "$HOME/.aliases"
-ln -s "$(pwd)/functions" "$HOME/.functions"
+if [ ! -h "$HOME/.aliases" ]; then
+  ln -s "$(pwd)/aliases" "$HOME/.aliases"
+fi
+if [ ! -h "$HOME/.functions" ]; then
+  ln -s "$(pwd)/functions" "$HOME/.functions"
+fi
+if [ ! -h "$HOME/.vimrc.bundles" ]; then
+  ln -s "$(pwd)/vim/vimrc.bundles" "$HOME/.vimrc.bundles"
+fi
 
 # ZSH
-ln -s "$(pwd)/zshrc" "$HOME/.zshrc"
+if [ ! -h "$HOME/.zshrc" ]; then
+  ln -s "$(pwd)/zshrc" "$HOME/.zshrc"
+fi
 
 # The Silver Searcher
 brew install the_silver_searcher
